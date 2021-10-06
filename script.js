5b065929d0b836ef99b7b43672da932472013e17
HEGADE <subramanyashegade@gmail.com>
2021-10-06 22:16:56 +0530

    added blocking functionality

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ let siteUrl = document.getElementById("siteUrl");
 let sitesToDisplay = document.getElementById("site_list");
 let siteUrlArray = [];
 
-let siteDataObj = {};
 const addSite = "Add site";
-const key = "ninja";
-const value = "ninja1";
-let so = LocalStore.getItem();
-
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let url = tabs[0].url;
+  siteUrl.value = url.split("/")[2];
+});
 Array.from(functionalButtons).forEach((ele) => {
   ele.addEventListener("click", (e) => {
     if (e.currentTarget.innerText === addSite) {
@@ -26,13 +25,12 @@ Array.from(functionalButtons).forEach((ele) => {
       addSiteButton.style.border = "2.3px solid #00a2ff";
       e.currentTarget.style.border = "2.3px solid red";
       showOrHide(true);
-      chrome.storage.local.get("key",  function (result) {
-        console.log("mmere", result);
-        result.key.forEach((site) => {
+      chrome.storage.local.get("key", function (result) {
+        if (!result?.key) return;
+          result.key.forEach((site) => {
           let li = document.createElement("li");
           li.innerText = site;
           sitesToDisplay.append(li);
-          console.log("lolol",site);
         });
       });
     }
@@ -45,20 +43,25 @@ function showOrHide(determine) {
 }
 addButton.addEventListener("click", (e) => {
   chrome.storage.local.get("key", async function (result) {
-
-    if(result?.key){
-      alert("ha",result.key)
-
-      console.log(result.key,"hello mother")
-
+    if (result?.key) {
       siteUrlArray.push(...result?.key);
     }
     console.log("before", siteUrlArray);
-    siteUrlArray.push(siteUrl.value);
-    console.log("after", siteUrlArray);
-  
+    if (!siteUrl.value.includes("www")) {
+      siteUrlArray.push("www." + siteUrl.value);
+    } else {
+      siteUrlArray.push(siteUrl.value);
+    }
+
     LocalStore.setItem(siteUrlArray);
-    return
+    alert("Added site to focused list");
+    // for page refreshing page
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (arrayOfTabs) {
+        chrome.tabs.reload(arrayOfTabs[0].id);
+      }
+    );
+    return;
   });
-  
 });
